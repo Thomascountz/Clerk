196b2de7eb781a6697db13b214843312384f0e63
Thomas Countz <thomascountz@gmail.com>
2017-08-21 08:59:04 -0400

    restart

--- app/models/user.rb
+++
@@ -1,6 +0,0 @@
-class User < ApplicationRecord
-  devise :database_authenticatable, :registerable,
-         :rememberable, :validatable
-
-  validates :full_name, presence: true, length: { maximum: 70 }
-end
--- spec/models/user_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe User, type: :model do
-  it { should validate_presence_of(:full_name) }
-  it { should validate_length_of(:full_name).is_at_most(70)}
-end
