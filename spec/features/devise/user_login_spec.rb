2dda9d309a744de953f05ee62643e69a0a69cbe9
Thomas Countz <thomascountz@gmail.com>
2017-08-21 11:50:07 -0400

    greenlit user sign out userflow

--- spec/features/devise/user_login_spec.rb
+++ spec/features/devise/user_login_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 RSpec.feature 'User log in', type: :feature do
 
 	let(:user) { create(:user) }
-	before { visit '/' }
+	before(:each) { visit '/' }
 
 	describe 'An existing user logging into their account' do
 		it 'shows a message that login was successful' do
@@ -11,6 +11,15 @@ RSpec.feature 'User log in', type: :feature do
 			page.fill_in('Password', with: user.password, :match => :prefer_exact)
 			page.click_button('Log in')
 			expect(page).to have_content('Signed in successfully.')
+		end
+	end
+
+	describe 'A signed in user logging out of their account' do
+		it 'shows a message that log out was successful' do
+			login_as(user)
+			visit '/'
+			page.click_link('Sign out')
+			expect(page).to have_content('Signed out successfully.')
 		end
 	end
 
