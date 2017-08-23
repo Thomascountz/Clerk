62e866731a26bdc38e31a7881ac239522b5339f1
Thomas Countz <thomascountz@gmail.com>
2017-08-23 11:06:40 -0400

    annotate(d) user model and factory

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                  :integer          not null, primary key
+#  email               :string           default(""), not null
+#  encrypted_password  :string           default(""), not null
+#  remember_created_at :datetime
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#
+
 FactoryGirl.define do
   factory :user do
     email { FFaker::Internet.safe_email }
