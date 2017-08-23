ee627be2c008b5e2e079290df2063dcae47b0909
Thomas Countz <thomascountz@gmail.com>
2017-08-23 19:52:08 -0400

    added authenticated user root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+	
+	authenticated :user do
+		root 'expenses#index'
+	end
+	root 'static_pages#index'
+
   devise_for :users
   resources :expenses, only: [:index, :new, :create]
-	root 'static_pages#index'
 end
